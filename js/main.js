6a4fe6336aeb280f61c09a701d2ea3afd01be9ce
Paula-333 <71704680+Paula-333@users.noreply.github.com>
2020-10-18 20:03:11 +0200

    errores medio solucionados

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 //Elegir personajes
 let arrayPlayers = [];
+class Player {
+    constructor(nombre, ataque, defensa) {
+    
+    this.nombre = nombre;
+    this.vida = 100;
+    this.ataque = ataque;
+    this.defensa = defensa;
+    }
+}
+    
+    let Kourt = new Player("Kourt",60,50);
+    let Kim = new Player("Kim",55,50);
+    let Khloe = new Player("Khloe",65,55);
+    let Kylie = new Player("Kylie",60,45);
+
+
 //document.getElementById("botonFight").style.visibility = "hidden";
 const selectPlayer = (player) => { 
     
     if(arrayPlayers.length < 2){
         
-        arrayPlayers.push(player);}
+        if (player == 'Kourt') {
+            arrayPlayers.push(Kourt);
+        } else if (player == 'Kim') {
+            arrayPlayers.push(Kim);
+        } else if (player == 'Khloe') { 
+            arrayPlayers.push(Khloe);
+        } else if (player == 'Kylie') {
+            arrayPlayers.push(Kylie);
+    }
+
         
     if(arrayPlayers.length == 2){
             console.log("Ya has escogido los 2 personajes",arrayPlayers);
@@ -18,7 +43,7 @@ const selectPlayer = (player) => {
 
 //Al elegir los personajes se esconden los no selecionados al pulsar start
 
-const botonStart = () => {
+function botonStart () {
 let escogidoKourtney = false;
 let escogidoKim = false;
 let escogidoKhloe = false;
@@ -26,15 +51,22 @@ let escogidoKylie = false;
 
 if(arrayPlayers.includes('Kourtney')){
     escogidoKourtney = true;
+    arrayPlayers.push(Kourt);
 }
 if(arrayPlayers.includes('Kim')){
     escogidoKim = true;
+    arrayPlayers.push(Kim);
+
 }
 if(arrayPlayers.includes('Khloe')){
     escogidoKhloe = true;
+    arrayPlayers.push(Khloe);
+
 }  
 if(arrayPlayers.includes('Kylie')){
     escogidoKylie = true;
+    arrayPlayers.push(Kylie);
+
 }
 
 if(!escogidoKourtney){
@@ -59,3 +91,37 @@ document.getElementById("botonStart").style.visibility = "hidden";
 
 }
 
+//JUGADORES
+let playersDiv = document.getElementById('players');
+let life1 = document.getElementById('life1');
+let life2 = document.getElementById('life2');
+let winner = document.getElementById('winner');
+
+
+    function fight (atack) {
+     
+        if (atack == 0){
+            if ((life2.value + arrayPlayers[0].defensa)- arrayPlayers[1].ataque>=0){life2.value -= arrayPlayers[1].ataque - arrayPlayers[0].defensa;
+            }else{
+                life2.value = 0;
+                finish (0);
+            }
+    
+            }else{
+                if ((life1.value + arrayPlayers[1].defensa)- arrayPlayers[0].ataque>=0){life1.value -= arrayPlayers[0].ataque - arrayPlayers[1].defensa;
+                }else{
+                    life1.value = 0;
+                    finish (1);
+                }
+        }
+    }    
+
+
+function finish () {
+    playersDiv.style.visibility = 'hidden';
+    winner.style.visibility = 'visible';
+    
+
+    
+}
+}
